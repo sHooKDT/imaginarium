ac268f34d10cd7391a1d798af9167f1da325d5cd
sHooK <shook@live.ru>
2015-05-11 01:09:05 +0500

    Added join form
    +Configured ngShow for form
    +Added join function in angular context

--- client/js/main.js
+++ client/js/main.js
@@ -2,8 +2,17 @@ var IGame = angular.module('imaginarium', []);
 
 IGame.controller('page-switcher', function($scope) {
 
-    $scope.pcount = 7;
+    $scope.playerscount = 7;
     $scope.pages = ['page-lobby', 'page-wait', 'page-turn', 'page-main-turn', 'page-vote', 'page-score'];
     $scope.activepage = $scope.pages[0];
 
  })
+
+IGame.controller('player-join', function($scope) {
+	$scope.playername = '';
+    $scope.formvisible = false;
+
+    $scope.joingame = function () {
+    	console.log('Player wants to join game with name ' + $scope.playername)
+    }
+})
