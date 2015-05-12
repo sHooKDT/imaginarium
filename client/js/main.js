1328e7e292f2e505c522e7326cec81f679e3411a
sHooK <shook@live.ru>
2015-05-12 21:37:34 +0500

    Last pages completed (except score), front-end rd

--- client/js/main.js
+++ client/js/main.js
@@ -1,9 +1,11 @@
-var IGame = angular.module('imaginarium', ['ngAnimate']);
+var IGame = angular.module('imaginarium', ['ngAnimate'])
+
 
 IGame.controller('page-switcher', function($scope) {
 
+	example_scope = $scope
     $scope.playerscount = 7;
-    $scope.pages = ['page-lobby', 'page-wait', 'page-turn', 'page-main-turn', 'page-vote', 'page-score'];
+    $scope.pages = ['page-lobby', 'page-wait', 'page-turn', 'page-vote', 'page-score'];
     $scope.activepage = $scope.pages[3];
 
  })
@@ -19,12 +21,32 @@ IGame.controller('player-join', function($scope) {
 
 })
 
-IGame.controller('card-controller', function($scope) {
+IGame.controller('turn-controller', function($scope) {
 
 	$scope.cards = [0, 44, 72, 144, 2, 1]
+	$scope.selectedcard = undefined
+	$scope.overlayon = false
+	$scope.cardass = ''
 
 	$scope.clickcard = function (c_id) {
-		console.log('Player chosen card with id: ' + c_id)
+		console.log('Player selected card: ' + c_id)
+		$scope.selectedcard = c_id
+		$scope.overlayon = true
 	}
 
+	$scope.choice = function () {
+		console.log('Player selected card ' + $scope.selectedcard + ' and wrote assocication: ' + $scope.cardass)
+	}
+})
+
+IGame.controller('vote-controller', function($scope) {
+
+	$scope.table = [5, 6, 7, 99, 555, 2]
+	$scope.selectedcard = undefined
+
+	$scope.clickcard = function (c_id) {
+		console.log('Player selected card: ' + c_id)
+		$scope.selectedcard = c_id
+		$scope.overlayon = true
+	}
 })
